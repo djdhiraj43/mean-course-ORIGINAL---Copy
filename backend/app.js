d42f124d252e3bd01510758dab514313063dd1d1
Dhiraj Borse <dj.dhiraj43@gmail.com>
2019-05-20 21:33:04 +0530

    Commit on 20 May 2019

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,46 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 const app = express();
 
-app.use((req,res, next) => {
-  console.log('First middleware');
-  next();
-});
+mongoose.connect("mongodb+srv://admin:admin@cluster0-h5lhb.mongodb.net/node-angular?retryWrites=true")
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  })
 
-app.use((req,res, next) => {
-  res.send('Hello from the express');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+})
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
+  res.status(201).json({
+    message:"Post added successfully"
+  });
+})
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
+  });
+  
 });
 
 module.exports = app;
