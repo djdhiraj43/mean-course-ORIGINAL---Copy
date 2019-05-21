b344c6e1c48e168cbfa0a6b34bd4d6a4db8875be
Dhiraj Borse <dj.dhiraj43@gmail.com>
2019-05-21 14:01:16 +0530

    commited on 21 May 2019

--- backend/app.js
+++ backend/app.js
@@ -27,10 +27,13 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message:"Post added successfully"
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message:"Post added successfully",
+      postId: createdPost._id
+    });
   });
+
 })
 
 app.get("/api/posts", (req, res, next) => {
@@ -41,6 +44,13 @@ app.get("/api/posts", (req, res, next) => {
     });
   });
   
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted!"});
+  })
 });
 
 module.exports = app;
