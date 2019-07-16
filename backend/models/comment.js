64436a07d7932552c178caf2e94e1aef0344263b
dj.dhiraj43 <dj.dhiraj43@gmail.com>
2019-07-16 17:46:10 +0530

    --- Auto-Git Commit ---

---
+++ backend/models/comment.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    postId: { type: String, required: true },
+    createdDate: { type: Date, required: true },
+    comment: { type: String, required: true },
+    authorName: { type: String, required: true },
+    comments: { type: mongoose.Schema.Types.Array}
+})
+
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ backend/routes/comments.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const CommentsController = require("../controllers/comments")
+//const checkAuth = require('../middleware/check-auth');
+//const extractFile = require('../middleware/file');
+const router = express.Router();
+
+//We are only passing the reference of functions like createPost and not executing them - createPost() because it should not be executed immediately but only when there is a re
+  
+  
+  router.get("/:postId", CommentsController.getComments);
+  
+
+  module.exports = router;
