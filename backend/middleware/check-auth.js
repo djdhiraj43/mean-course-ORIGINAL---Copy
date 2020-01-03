3b6408d5a337c3c9e3695cf4b560165f6378fdfd
dj.dhiraj43 <dj.dhiraj43@gmail.com>
2020-01-03 23:40:17 +0530

    --- Auto-Git Commit ---

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
     const token = req.headers.authorization.split(" ")[1]; //token has a word Bearer before the token by convention hence we are splitting it to extract the token
-    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    const decodedToken = jwt.verify(token, "vhsfkfhdjh346834743urv3rvrgdvfjhs");
+    //const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = {email: decodedToken.email, userId: decodedToken.userId};
     next();
     } catch (error) {
